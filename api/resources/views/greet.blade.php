e934ffc5eea73ddefcbca3e9194c0be9e2b61c85
shaun deshler <shaundeshler@shauns-MacBook-Pro.local>
2020-02-22 17:19:41 -0800

    initial commit

---
+++ api/resources/views/greet.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="./sass/app.scss">
+    <title></title>
+  </head>
+  <body>
+    <h1>Log in
+  </body>
+</html>
---
+++ api/routes/web.php
@@ -0,0 +1,38 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    //return view('greet');
+    return view('welcome');
+});
+
+Route::get('/greet', function(){
+      // From URL to get webpage contents.
+    $url = "https://swapi.co/api/people/";
+
+    // Initialize a CURL session.
+    $ch = curl_init();
+
+    // Return Page contents.
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+    //grab URL and pass it to the variable.
+    curl_setopt($ch, CURLOPT_URL, $url);
+
+    $result = curl_exec($ch);
+
+    $parsed = json_decode($result, true);
+
+    return $result;
+  //return view('greet');
+});
